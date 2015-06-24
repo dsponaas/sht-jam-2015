ec8ffa5c8bc932e95819daa9529970c56b7fd61d
sponezzis <dsponaas@gmail.com>
2015-06-23 20:04:52 -0400

    initial commit

---
+++ desktop/src/com/sponezzis/shtjam2015/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sponezzis.shtjam2015.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sponezzis.shtjam2015.ShtJam2015;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ShtJam2015(), config);
+	}
+}
