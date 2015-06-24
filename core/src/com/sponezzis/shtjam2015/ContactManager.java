5051c23bb60cb75aacc782bb44274c4477b69ade
sponezzis <dsponaas@gmail.com>
2015-06-24 01:09:17 -0400

    remove player bullets that strike level bounds

--- core/src/com/sponezzis/shtjam2015/ContactManager.java
+++ core/src/com/sponezzis/shtjam2015/ContactManager.java
@@ -2,6 +2,7 @@ package com.sponezzis.shtjam2015;
 
 import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.physics.box2d.*;
 import com.sponezzis.shtjam2015.components.BodyComponent;
 
@@ -22,7 +23,21 @@ public class ContactManager implements ContactListener {
 
     @Override
     public void beginContact(Contact contact) {
+        Fixture fixtureA = contact.getFixtureA();
+        Fixture fixtureB = contact.getFixtureB();
+        Body bodyA = fixtureA.getBody();
+        Body bodyB = fixtureB.getBody();
+        short fixtureAType = fixtureA.getFilterData().categoryBits;
+        short fixtureBType = fixtureB.getFilterData().categoryBits;
+        Entity entityA = (Entity)fixtureA.getUserData();
+        Entity entityB = (Entity)fixtureB.getUserData();
 
+        if((Constants.BITMASK_PLAYER_BULLET == fixtureAType) && (Constants.BITMASK_LEVEL_BOUNDS == fixtureBType)) {
+            EntityManager.getInstance().destroyEntity(entityA);
+        }
+        else if((Constants.BITMASK_PLAYER_BULLET == fixtureBType) && (Constants.BITMASK_LEVEL_BOUNDS == fixtureAType)) {
+            EntityManager.getInstance().destroyEntity(entityB);
+        }
     }
 
     @Override
